7c6a09ea17c862191758395181ab73b9a5382eef
Abhijit Barik <71961635+Abhijit-Barik01@users.noreply.github.com>
2021-02-23 10:23:20 +0530

    Add files via upload
    php coding file

---
+++ 9_variables.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$name = 'Abhijit';
+$number = 100;
+$Number_List = 100.5;
+//$num-ber= 400;
+//$0number = 500;
+//php case sensetive
+$NUMBER = 100;
+$NUMBeR = 100;
+//concatination by dot
+echo $name . " " . $NUMBeR;
+//we can assign html(text,image,link etc) in variabe
+$name = "<h1> HELLO</h1>";
+
+echo $name;
+
+
+?>
+
+
+
+</body>
+</html>
